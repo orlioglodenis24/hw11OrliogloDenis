a99068270ab591be9d47e829ba28fbf95f2cddca
OrliogloDenis <orlioglodenis24@gmail.com>
2024-04-05 20:28:18 +0300

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,88 @@
+class Animall {
+    int eyes;
+
+
+    Animall() {
+        System.out.println("I am Animal");
+    }
+
+
+    void eat() {
+        System.out.println("Animal eats");
+    }
+
+
+    void drink() {
+        System.out.println("Animal drinks");
+    }
+}
+
+
+class Pet extends Animall {
+    String name;
+    int tail = 1;
+    int paw = 4;
+
+
+    Pet() {
+        System.out.println("I am Pet");
+        eyes = 2;
+    }
+
+
+    void run() {
+        System.out.println("Pet runs");
+    }
+
+
+    void jump() {
+        System.out.println("Pet jumps");
+    }
+}
+
+
+class Dog extends Pet {
+
+    Dog(String name) {
+        System.out.println("I am dog and my name is: " + name);
+    }
+
+
+    void play() {
+        System.out.println("Dog plays");
+    }
+}
+
+
+class Hamster extends Pet {
+
+    Hamster(String name) {
+        System.out.println("I am a hamster and my name is: " + name);
+    }
+
+
+    void sleep() {
+        System.out.println("Hamster is sleeping");
+    }
+}
+
+// Главный класс
+public class Animal {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Denis");
+        Hamster hamster = new Hamster("Drovosek");
+
+        dog.eat();
+        dog.drink();
+        dog.run();
+        dog.jump();
+        dog.play();
+
+        hamster.eat();
+        hamster.drink();
+        hamster.run();
+        hamster.jump();
+        hamster.sleep();
+    }
+}
