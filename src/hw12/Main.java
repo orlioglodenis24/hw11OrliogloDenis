d55815beaac823235ff93b9c77278dd37b802128
OrliogloDenis <orlioglodenis24@gmail.com>
2024-04-08 16:13:56 +0300

    commit and Push final hw12

--- src/hw12/Main.java
+++ src/hw12/Main.java
@@ -5,11 +5,14 @@ class Main {
     public static void main(String[] args) {
         Car car = new Car("m1", 100);
         car.go();
+        car.calculateDistance(1000);
         Truck truck = new Truck("ms10", 120, 2000);
         truck.go();
+        truck.calculateDistance(100,3);
 
         Plane airplane = new Plane("airplane-9000 ", 200, 2000);
         airplane.go();
+        airplane.calculateDistance(10000,3);
     }
 
 }
