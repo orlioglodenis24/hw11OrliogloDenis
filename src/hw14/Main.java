2e1a2a6a5abe97df3e051b836f7f29e25e9312c5
OrliogloDenis <orlioglodenis24@gmail.com>
2024-04-18 18:11:03 +0300

    commit hw14

--- src/hw14/Main.java
+++ src/hw14/Main.java
@@ -2,14 +2,14 @@ package hw14;
 //Добавить в свои классы проверку и выкидывать исключения в случае ошибок
 //Обработать исключения в try catch
 
-public class Main {
-    public static void main(String[] args) {
-        MyClass obj = new MyClass();
-        try {
-            obj.setValue(-5);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Произошла ошибка: " + e.getMessage());
-        }
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//        MyClass obj = new MyClass();
+//        try {
+//            obj.setValue(-5);
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Произошла ошибка: " + e.getMessage());
+//        }
+//    }
+//}
 
--- src/hw14/MyClass.java
+++ src/hw14/MyClass.java
@@ -1,13 +1,29 @@
 package hw14;
+//Добавить в свои классы проверку и выкидывать исключения в случае ошибок
+//Обработать исключения в try catch
 
 public class MyClass {
-    private int myValue;
+    private int value;
 
-    public void setValue(int value) throws IllegalArgumentException {
-        if (value < 0) {
-            throw new IllegalArgumentException("Значение не может быть отрицательным");
+    public MyClass(int value) {
+        this.value = value;
+    }
+
+    public int divideBy(int divisor) throws ArithmeticException {
+        if (divisor == 0) {
+            throw new ArithmeticException("Division by zero");
         }
-        this.myValue = value;
+        return this.value / divisor;
+    }
+
+    public static void main(String[] args) {
+        try {
+            MyClass myObject = new MyClass(10);
+            int result = myObject.divideBy(0);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
 
