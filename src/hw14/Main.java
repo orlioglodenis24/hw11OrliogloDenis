e82483d57dfaa5c8c873445e8bb4edcf488b499a
OrliogloDenis <orlioglodenis24@gmail.com>
2024-04-18 20:18:58 +0300

    created class Main with psmv and removed the if construct hw14

--- src/hw14/Main.java
+++ src/hw14/Main.java
@@ -1,15 +1,14 @@
 package hw14;
 //Добавить в свои классы проверку и выкидывать исключения в случае ошибок
 //Обработать исключения в try catch
-
-//public class Main {
-//    public static void main(String[] args) {
-//        MyClass obj = new MyClass();
-//        try {
-//            obj.setValue(-5);
-//        } catch (IllegalArgumentException e) {
-//            System.out.println("Произошла ошибка: " + e.getMessage());
-//        }
-//    }
-//}
-
+public class Main {
+    public static void main(String[] args) {
+        try {
+            MyClass myObject = new MyClass(10);
+            int result = myObject.divideBy(0);
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
--- src/hw14/MyClass.java
+++ src/hw14/MyClass.java
@@ -10,20 +10,9 @@ public class MyClass {
     }
 
     public int divideBy(int divisor) throws ArithmeticException {
-        if (divisor == 0) {
-            throw new ArithmeticException("Division by zero");
-        }
         return this.value / divisor;
     }
 
-    public static void main(String[] args) {
-        try {
-            MyClass myObject = new MyClass(10);
-            int result = myObject.divideBy(0);
-            System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
+
 }
 
