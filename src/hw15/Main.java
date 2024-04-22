f053773a589d038580bce6efa4f16060d6b8e961
OrliogloDenis <orlioglodenis24@gmail.com>
2024-04-22 10:39:31 +0300

    updates on latest remarks

--- src/hw15/Main.java
+++ src/hw15/Main.java
@@ -5,15 +5,11 @@ public class Main {
         Car myCar = new Car("Toyota", "Red", 50);
 
         try {
+            myCar.safetyBelt("Not Belt");
             myCar.drive(0);
-        } catch (NotFuelException e) {
+        } catch (NotFuelException | NotBeltException e) {
             System.out.println("Exception caught: " + e.getMessage());
         }
 
-        try {
-            myCar.safetyBelt("Not Belt");
-        } catch (NotBeltException e) {
-            System.out.println("Exception caught: " + e.getMessage());
-        }
     }
 }
